16908aa87cefb71f97545d45aa3e331251e8fd35
Jack <jacktvicari@gmail.com>
2022-08-18 11:23:11 -0400

    Initial commit

---
+++ Assets/KeyChecker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyChecker : MonoBehaviour
+{
+    //public GameObject winMessage;
+    private bool isOpen;
+    public GameObject doorPivot;
+
+    private void Start()
+    {
+        isOpen = false;
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Key"))
+        {
+            //we won
+            if(!isOpen)
+            {
+                isOpen = true;
+                doorPivot.transform.Rotate(Vector3.up, 150f);
+            }
+        }
+    }
+    // Start is called before the first frame update
+
+}
---
+++ Assets/WinDetector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinDetector : MonoBehaviour
+{
+    public GameObject winMessage;
+
+    private void Start()
+    {
+        winMessage.SetActive(false);
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("MainCamera"))
+        {
+            //we won
+            if(!winMessage.activeInHierarchy)
+            {
+                winMessage.SetActive(true);
+            }
+        }
+    }
+    // Start is called before the first frame update
+
+}
