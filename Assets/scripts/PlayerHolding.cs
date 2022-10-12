6a7598507305954c758a3f6f9509759cfb80b318
Jack <54145187+ChaoticNutritional@users.noreply.github.com>
2022-10-12 16:47:55 -0400

    New assets for Custom Maze

---
+++ Assets/scripts/PlayerHolding.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHolding : MonoBehaviour
+{
+
+    public bool isHolding;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isHolding = false;
+    }
+
+    public bool
+}
---
+++ Assets/scripts/RevealTrap.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RevealTrap : MonoBehaviour
+{
+    [SerializeField]
+    private BoxCollider enterBox;
+
+    [SerializeField]
+    private GameObject falseFloors;
+
+    [SerializeField]
+    private GameObject playerObject;
+
+    [SerializeField]
+    private GameObject crossingPlank; //object required to be a child of player
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        //if player enters and floor has not yet been revealed
+        if (collision.gameObject == playerObject && falseFloors.activeSelf)
+        {
+            falseFloors.SetActive(false);
+        }
+
+        //if player enters and the floor has already been revealed
+        else if (collision.gameObject == playerObject && !falseFloors.activeSelf)
+        {
+            //if player doesn't have plank
+            if (collision.gameObject.GetComponent<>())
+            //output text that says "if only I had something I could use to cross this"
+
+            //if player has plank
+            crossingPlank.SetActive(true);
+        }
+    }
+}
